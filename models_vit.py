82178b72300f7505a2b15b85b420f56a6d300128
njiang2024 <159625067+njiang2024@users.noreply.github.com>
2024-06-15 23:08:04 +0800

    Update models_vit.py

--- models_vit.py
+++ models_vit.py
@@ -1,8 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-# Partly revised by YZ @UCL&Moorfields
-# --------------------------------------------------------
-
 from functools import partial
 
 import torch
