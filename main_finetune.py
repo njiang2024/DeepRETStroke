18404c8885a088f1d3b052ea20f6efbaea3b70b6
njiang2024 <159625067+njiang2024@users.noreply.github.com>
2024-06-15 23:07:16 +0800

    Update main_finetune.py

--- main_finetune.py
+++ main_finetune.py
@@ -1,8 +1,3 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
-# Partly revised by YZ @UCL&Moorfields
-# --------------------------------------------------------
-
 import argparse
 import datetime
 import json
@@ -35,28 +30,26 @@ import pickle
 
 from engine_finetune import train_one_epoch, evaluate
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0,1,2,3"
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
 
 def get_args_parser():
-	parser = argparse.ArgumentParser('MAE fine-tuning for image classification', add_help=False)
+	parser = argparse.ArgumentParser('DeepRETStroke System Development', add_help=False)
+
 	parser.add_argument('--batch_size', default=64, type=int,
 						help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-	parser.add_argument('--epochs', default=50, type=int)
+	parser.add_argument('--epochs', default=30, type=int)
+	parser.add_argument('--iterations', default=20, type=int)
 	parser.add_argument('--accum_iter', default=1, type=int,
 						help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
 	# Model parameters
 	parser.add_argument('--model', default='vit_large_patch16_s', type=str, metavar='MODEL',
 						help='Name of model to train')
-
 	parser.add_argument('--input_size', default=256, type=int,
 						help='images input size')
-
 	parser.add_argument('--drop_path', type=float, default=0.1, metavar='PCT',
 						help='Drop path rate (default: 0.1)')
-
-	parser.add_argument('--combined', action='store_true',
-						default=False,
+	parser.add_argument('--combined', action='store_true', default=False,
 						help="Model selection")
 
 	# Optimizer parameters
@@ -64,17 +57,14 @@ def get_args_parser():
 						help='Clip gradient norm (default: None, no clipping)')
 	parser.add_argument('--weight_decay', type=float, default=0.05,
 						help='weight decay (default: 0.05)')
-
 	parser.add_argument('--lr', type=float, default=None, metavar='LR',
 						help='learning rate (absolute lr)')
 	parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
 						help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
 	parser.add_argument('--layer_decay', type=float, default=0.75,
 						help='layer-wise lr decay from ELECTRA/BEiT')
-
 	parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
 						help='lower lr bound for cyclic schedulers that hit 0')
-
 	parser.add_argument('--warmup_epochs', type=int, default=10, metavar='N',
 						help='epochs to warmup LR')
 
@@ -115,17 +105,15 @@ def get_args_parser():
 						help='finetune from checkpoint')
 	parser.add_argument('--task', default='',type=str,
 						help='finetune from checkpoint')
-	parser.add_argument('--global_pool', action='store_true')
-	parser.set_defaults(global_pool=True)
+	parser.add_argument('--global_pool', action='store_true', default=True)
 	parser.add_argument('--cls_token', action='store_false', dest='global_pool',
 						help='Use class token instead of global pool for classification')
 
 	# Dataset parameters
-	parser.add_argument('--data_path', default='draft_data/test/gray_fundus', type=str,
+	parser.add_argument('--data_path', default='./data', type=str,
 						help='dataset path')
 	parser.add_argument('--nb_classes', default=1000, type=int,
 						help='number of the classification types')
-
 	parser.add_argument('--output_dir', default='./output_dir',
 						help='path where to save, empty for no saving')
 	parser.add_argument('--log_dir', default='./output_dir',
@@ -135,7 +123,6 @@ def get_args_parser():
 	parser.add_argument('--seed', default=0, type=int)
 	parser.add_argument('--resume', default='',
 						help='resume from checkpoint')
-
 	parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
 						help='start epoch')
 	parser.add_argument('--eval', action='store_true',
@@ -158,248 +145,6 @@ def get_args_parser():
 
 	return parser
 
-
-def main_0(args):
-	misc.init_distributed_mode(args)
-
-	print('job dir: {}'.format(os.path.dirname(os.path.realpath(__file__))))
-	print("{}".format(args).replace(', ', ',\n'))
-
-	device = torch.device(args.device)
-
-	# fix the seed for reproducibility
-	seed = args.seed + misc.get_rank()
-	torch.manual_seed(seed)
-	np.random.seed(seed)
-
-	cudnn.benchmark = True
-
-	dataset_train = build_dataset(is_train='train', args=args)
-	dataset_val = build_dataset(is_train='val', args=args)
-	dataset_test = build_dataset(is_train='test', args=args)
-
-	if True:  # args.distributed:
-		num_tasks = misc.get_world_size()
-		global_rank = misc.get_rank()
-		sampler_train = torch.utils.data.DistributedSampler(
-			dataset_train, num_replicas=num_tasks, rank=global_rank, shuffle=True
-		)
-		print("Sampler_train = %s" % str(sampler_train))
-		if args.dist_eval:
-			if len(dataset_val) % num_tasks != 0:
-				print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-					  'This will slightly alter validation results as extra duplicate entries are added to achieve '
-					  'equal num of samples per-process.')
-			sampler_val = torch.utils.data.DistributedSampler(
-				dataset_val, num_replicas=num_tasks, rank=global_rank, shuffle=True)  # shuffle=True to reduce monitor bias
-		else:
-			sampler_val = torch.utils.data.SequentialSampler(dataset_val)
-			
-		if args.dist_eval:
-			if len(dataset_test) % num_tasks != 0:
-				print('Warning: Enabling distributed evaluation with an eval dataset not divisible by process number. '
-					  'This will slightly alter validation results as extra duplicate entries are added to achieve '
-					  'equal num of samples per-process.')
-			sampler_test = torch.utils.data.DistributedSampler(
-				dataset_test, num_replicas=num_tasks, rank=global_rank, shuffle=True)  # shuffle=True to reduce monitor bias
-		else:
-			sampler_test = torch.utils.data.SequentialSampler(dataset_test)
-			
-
-	if global_rank == 0 and args.log_dir is not None and not args.eval:
-		os.makedirs(args.log_dir, exist_ok=True)
-		log_writer = SummaryWriter(log_dir=args.log_dir+args.task)
-	else:
-		log_writer = None
-
-	data_loader_train = torch.utils.data.DataLoader(
-		dataset_train, sampler=sampler_train,
-		batch_size=args.batch_size,
-		num_workers=args.num_workers,
-		pin_memory=args.pin_mem,
-		drop_last=True,
-	)
-
-	data_loader_val = torch.utils.data.DataLoader(
-		dataset_val, sampler=sampler_val,
-		batch_size=args.batch_size,
-		num_workers=args.num_workers,
-		pin_memory=args.pin_mem,
-		drop_last=False
-	)
-
-	data_loader_test = torch.utils.data.DataLoader(
-		dataset_test, sampler=sampler_test,
-		batch_size=args.batch_size,
-		num_workers=args.num_workers,
-		pin_memory=args.pin_mem,
-		drop_last=False
-	)
-	
-	
-	mixup_fn = None
-	mixup_active = args.mixup > 0 or args.cutmix > 0. or args.cutmix_minmax is not None
-	if mixup_active:
-		print("Mixup is activated!")
-		mixup_fn = Mixup(
-			mixup_alpha=args.mixup, cutmix_alpha=args.cutmix, cutmix_minmax=args.cutmix_minmax,
-			prob=args.mixup_prob, switch_prob=args.mixup_switch_prob, mode=args.mixup_mode,
-			label_smoothing=args.smoothing, num_classes=args.nb_classes)
-	
-	model = models_vit.__dict__[args.model](
-		num_classes=args.nb_classes,
-		drop_path_rate=args.drop_path,
-		global_pool=args.global_pool,
-		pre_train = args.pre_train,
-		progression = args.progression,
-		combined = args.combined
-	)
-
-	if args.finetune and not args.eval:
-		checkpoint = torch.load(args.finetune, map_location='cpu')
-
-		print("Load pre-trained checkpoint from: %s" % args.finetune)
-		checkpoint_model = checkpoint['model'] # 加载的模型权重字典
-		# state_dict = model.state_dict() # 任务需要的模型权重模板
-
-		# interpolate position embedding
-		interpolate_pos_embed(model, checkpoint_model)
-
-		# load pre-trained model
-		msg = model.load_state_dict(checkpoint_model, strict=False)
-		print(msg)
-
-		if args.global_pool:
-			#assert set(msg.missing_keys) == {'head.weight', 'head.bias', 'fc_norm.weight', 'fc_norm.bias'}
-			pass
-		else:
-			#assert set(msg.missing_keys) == {'head.weight', 'head.bias'}
-			pass
-
-		# manually initialize fc layer
-		trunc_normal_(model.hidden_layer.weight, std=2e-5)
-		if args.combined:
-			trunc_normal_(model.meta_layer.weight, std=2e-5)
-			trunc_normal_(model.head_c2.weight, std=2e-5)
-			trunc_normal_(model.head_c5.weight, std=2e-5)
-			for name, para in model.named_parameters():
-				if "meta_layer" in name or "head_c" in name:
-					para.requires_grad = True
-				else:
-					para.requires_grad = False
-		else:
-			trunc_normal_(model.head_2.weight, std=2e-5)
-			trunc_normal_(model.head_5.weight, std=2e-5)
-
-	model.to(device)
-
-	model_without_ddp = model
-	n_parameters = sum(p.numel() for p in model.parameters() if p.requires_grad)
-
-	print("Model = %s" % str(model_without_ddp))
-	print('number of params (M): %.2f' % (n_parameters / 1.e6))
-
-	eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
-	
-	if args.lr is None:  # only base_lr is specified
-		args.lr = args.blr * eff_batch_size / 256
-
-	print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
-	print("actual lr: %.2e" % args.lr)
-
-	print("accumulate grad iterations: %d" % args.accum_iter)
-	print("effective batch size: %d" % eff_batch_size)
-
-	if args.distributed:
-		model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu], find_unused_parameters=True)
-		model_without_ddp = model.module
-
-	# build optimizer with layer-wise lr decay (lrd)
-	param_groups = lrd.param_groups_lrd(model_without_ddp, args.weight_decay,
-		no_weight_decay_list=model_without_ddp.no_weight_decay(),
-		layer_decay=args.layer_decay
-	)
-	optimizer = torch.optim.AdamW(param_groups, lr=args.lr)
-	loss_scaler = NativeScaler()
-
-	if mixup_fn is not None:
-		# smoothing is handled with mixup label transform
-		criterion = SoftTargetCrossEntropy()
-	else:
-		if args.pre_train or args.progression == False:
-			if args.smoothing > 0.:
-				criterion = LabelSmoothingCrossEntropy(smoothing=args.smoothing)
-			else:
-				criterion = torch.nn.CrossEntropyLoss()
-		else:
-			criterion = torch.nn.BCELoss(reduction="sum")
-
-	print("criterion = %s" % str(criterion))
-
-	misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
-
-	if args.eval:
-		test_stats,auc_roc,df_result = evaluate(data_loader_test, model, device, args.task, epoch=0, mode='test',
-												pre_train=args.pre_train, progression=args.progression,
-												combined=args.combined)
-		result_path = os.path.join(args.task, "results.csv")
-		df_result.to_csv(result_path)
-		exit(0)
-
-	print(f"Start training for {args.epochs} epochs")
-	start_time = time.time()
-	max_accuracy = 0.0
-	max_auc = 0.0
-	for epoch in range(args.start_epoch, args.epochs):
-		if args.distributed:
-			data_loader_train.sampler.set_epoch(epoch)
-		train_stats = train_one_epoch(
-			model, data_loader_train,
-			optimizer, device, epoch, loss_scaler,
-			args.clip_grad, mixup_fn,
-			log_writer=log_writer,
-			args=args
-		)
-  
-		val_stats,val_auc_roc = evaluate(data_loader_val, model, device,args.task,epoch, mode='val',
-										 pre_train=args.pre_train, progression=args.progression,
-										 combined=args.combined)
-
-		misc.save_model(
-			args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-			loss_scaler=loss_scaler, epoch=epoch)
-
-		if max_auc<val_auc_roc:
-			max_auc = val_auc_roc
-			
-			if args.output_dir:
-				misc.save_model(
-					args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-					loss_scaler=loss_scaler, epoch=epoch, best_weight=True)
-
-		if epoch==(args.epochs-1):
-			#test_stats,auc_roc = evaluate(data_loader_test, model, device,args.task,epoch, mode='test',num_class=args.nb_classes)
-			pass
-
-		if log_writer is not None:
-			log_writer.add_scalar('perf/val_acc1', val_stats['acc1'], epoch)
-			log_writer.add_scalar('perf/val_auc', val_auc_roc, epoch)
-			log_writer.add_scalar('perf/val_loss', val_stats['loss'], epoch)
-			
-		log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
-						'epoch': epoch,
-						'n_parameters': n_parameters}
-
-		if args.output_dir and misc.is_main_process():
-			if log_writer is not None:
-				log_writer.flush()
-			with open(os.path.join(args.output_dir, "log.txt"), mode="a", encoding="utf-8") as f:
-				f.write(json.dumps(log_stats) + "\n")
-
-				
-	total_time = time.time() - start_time
-	total_time_str = str(datetime.timedelta(seconds=int(total_time)))
-	print('Training time {}'.format(total_time_str))
 
 def main(args):
 	misc.init_distributed_mode(args)
@@ -437,6 +182,8 @@ def main(args):
 		drop_path_rate=args.drop_path,
 		global_pool=args.global_pool,
 	)
+
+	# two classifiers for SBI Detector
 	det1 = LogisticRegression(class_weight="balanced", max_iter=2000)
 	det2 = LogisticRegression(class_weight="balanced", max_iter=2000)
 
@@ -494,18 +241,20 @@ def main(args):
 
 #############################################################################################
 
+	# csv files for constructing the training and validation set
 	dft_sb_tr = pd.read_csv("data/df_sb_tr.csv")
 	dft_st_tr = pd.read_csv("data/df_st_tr.csv")
 	dft_sb_va = pd.read_csv("data/df_sb_va.csv")
 	dft_st_va = pd.read_csv("data/df_st_va.csv")
-	#dft_sb_te = pd.DataFrame([])
-	#dft_st_te = pd.DataFrame([])
 
+	# define the weight of pos/neg sample and the standard of high confidence sample
 	weight = [1, 4]
 	pos_thr = 0.75
 	neg_thr = 0.75
+
 	path = args.data_path
 
+	# construct the datasets for different stages of training
 	dataset_st_tr_s1 = build_dataset('train', dft_st_tr, 1, data_path=path)
 	dataset_st_va_s1 = build_dataset('test', dft_st_va, 1, data_path=path)
 	dataset_st_tr_e = build_dataset('test', dft_st_tr, "e", data_path=path)
@@ -556,14 +305,14 @@ def main(args):
 		drop_last=False
 	)
 
-	# step 1
+	# step 1, model initialization
 	stage = 1
 
 	print(f"Start training stage 1")
 	start_time = time.time()
 	max_auc_s1 = 0.0
 
-	for epoch in range(1):
+	for epoch in range(30):
 		if args.distributed:
 			data_loader_st_tr_s1.sampler.set_epoch(epoch)
 		train_stats = train_one_epoch(
@@ -586,11 +335,8 @@ def main(args):
 					args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
 					loss_scaler=loss_scaler, epoch=epoch, best_weight=True, stage=stage)
 
-
 		if log_writer is not None:
-			#log_writer.add_scalar('perf/val_acc1', val_stats['acc1'], epoch)
 			log_writer.add_scalar('perf/val_auc', val_auc_roc, epoch)
-			#log_writer.add_scalar('perf/val_loss', val_stats['loss'], epoch)
 
 		log_stats = {**{f'train_{k}': v for k, v in train_stats.items()},
 					 'epoch': epoch,
@@ -606,17 +352,19 @@ def main(args):
 	total_time_str = str(datetime.timedelta(seconds=int(total_time)))
 	print('Stage 1 Training time {}'.format(total_time_str))
 
+##########################   model initialization end   ############################
+
 	repeat_performance_sb = [[],[]]
 	repeat_performance_st = []
 
-	for repeat in range(3):
-		# stage e
+	for repeat in range(30):
+		# stage e, generate the features from the frozed encoder
 		stage = "e"
 
 		if repeat == 0:
 			checkpoint = torch.load(args.task+'checkpoint-1-best.pth', map_location='cpu')
 		else:
-			checkpoint = torch.load(args.task + 'checkpoint-4-r%d-best.pth'%(repeat), map_location='cpu')
+			checkpoint = torch.load(args.task + 'checkpoint-4-r%s-best.pth'%(str(repeat-1)), map_location='cpu')
 		model.load_state_dict(checkpoint['model'], strict=False)
 		model.to(device)
 
@@ -629,7 +377,7 @@ def main(args):
 		dataset_sb_tr_f0 = evaluate(data_loader_sb_tr_e, model, device, args.task, stage, mode="test")
 		dataset_sb_va_f0 = evaluate(data_loader_sb_va_e, model, device, args.task, stage, mode="test")
 
-		# stage 2
+		# stage 2, semi-supervised learning
 		stage = 2
 
 		print(f"Start training stage 2")
@@ -694,13 +442,10 @@ def main(args):
 			pos_sft = sorted(pos_sft0 - neg_sft0)
 			neg_sft = sorted(neg_sft0 - pos_sft0)
 			res_sft = sorted(set(np.arange(dataset_st_tr_f.shape[0])) - set(pos_sft) - set(neg_sft))
-			print(res_sft)
 
 			if len(pos_sft) == 0 or len(neg_sft) == 0 or len(pos_sft + neg_sft) == dataset_st_tr_f.shape[0]:
 				repeat_performance_sb[0].append(roc_auc_score(gt_sb_va, pre_sb_va_1))
 				repeat_performance_sb[1].append(roc_auc_score(gt_sb_va, pre_sb_va_2))
-				print(pos_sft)
-				print(neg_sft)
 				break
 			else:
 				dataset_st_sb_0 = dataset_st_tr_f.iloc[pos_sft, 0:ft_shape * 2 + 1]
@@ -767,7 +512,7 @@ def main(args):
 				if args.output_dir:
 					misc.save_model(
 						args=args, model=model, model_without_ddp=model_without_ddp, optimizer=optimizer,
-						loss_scaler=loss_scaler, epoch=epoch, best_weight=True, stage=stage, repeat=repeat)
+						loss_scaler=loss_scaler, epoch=epoch, best_weight=True, stage=stage, repeat=str(repeat))
 			else:
 				ctimes = ctimes + 1
 				if ctimes > ptimes:
@@ -800,5 +545,3 @@ if __name__ == '__main__':
 	if args.output_dir:
 		Path(args.output_dir).mkdir(parents=True, exist_ok=True)
 	main(args)
-
-
